c26d00be68a108a940019a2365bc0a2e170668bb
JD <jd@csh.rit.edu>
2015-02-20 14:36:52 -0500

    Changed the bulk insert to use avro data instead

--- src/main/java/edu/rit/csh/wikiData/Driver.java
+++ src/main/java/edu/rit/csh/wikiData/Driver.java
@@ -16,6 +16,18 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
+import edu.rit.csh.LogRecord;
+
+import org.apache.avro.*;
+import org.apache.avro.Schema.Type;
+import org.apache.avro.mapreduce.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
 
 /**
  * HBase bulk import example<br>
@@ -26,37 +38,43 @@ import org.apache.hadoop.util.GenericOptionsParser;
  * <li>args[2]: HBase table name
  * </ol>
  */
-public class Driver {
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
+public class Driver extends Configured implements Tool {
+
+    public int run(String[] args) throws Exception {
+    Configuration conf = getConf();
+    //Configuration conf = new Configuration();
     args = new GenericOptionsParser(conf, args).getRemainingArgs();
 
     conf.setInt("epoch.seconds.tipoff", 1275613200);
     conf.set("hbase.table.name", args[2]);
+    
+    // Deletes the output dir
+    FileSystem fs = FileSystem.get(conf);
+    fs.delete(new Path(args[1]), true);
     
     // Load hbase-site.xml 
     HBaseConfiguration.addHbaseResources(conf);
 
     Job job = new Job(conf, "HBase Bulk Import of Wiki Data");
     job.setJarByClass(HBaseKVMapper.class);
-
-    job.setMapperClass(HBaseKVMapper.class);
-    job.setMapOutputKeyClass(ImmutableBytesWritable.class);
-    job.setMapOutputValueClass(KeyValue.class);
-
-    job.setInputFormatClass(TextInputFormat.class);
-
-    HTable hTable = new HTable(conf, args[2].getBytes());
     
-    // Auto configure partitioner and reducer
-    HFileOutputFormat.configureIncrementalLoad(job, hTable);
-
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+    job.setInputFormatClass(AvroKeyInputFormat.class);
+    job.setMapperClass(HBaseKVMapper.class);
+    AvroJob.setInputKeySchema(job, LogRecord.getClassSchema());
+    job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+    job.setMapOutputValueClass(KeyValue.class);
+    //AvroJob.setOutputKeySchema(job, Schema.create(Schema.Type.BYTES));
+    //AvroJob.setOutputValueSchema(job, Schema.create(Schema.Type.INT));
+
+    // Auto configure partitioner and reducer
+    HTable hTable = new HTable(conf, args[2].getBytes());
+    HFileOutputFormat.configureIncrementalLoad(job, hTable);
+
     job.waitForCompletion(true);
 
-    FileSystem fs = FileSystem.get(conf);
     //fs.setPermission(new Path(args[1]), FsPermission.valueOf("drwxrwxrwx"));
     Runtime rt = Runtime.getRuntime();
     rt.exec("hadoop fs -chmod -R 777 " + args[1]);
@@ -66,5 +84,11 @@ public class Driver {
     loader.doBulkLoad(new Path(args[1]), hTable);
 
     fs.delete(new Path(args[1]), true);
+    return 0;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new Driver(), args);
+    System.exit(res);
   }
 }
--- src/main/java/edu/rit/csh/wikiData/HBaseKVMapper.java
+++ src/main/java/edu/rit/csh/wikiData/HBaseKVMapper.java
@@ -2,33 +2,42 @@ package edu.rit.csh.wikiData;
 
 import java.io.IOException;
 import java.util.Locale;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
+
+import org.apache.avro.Schema;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
+import org.apache.avro.generic.GenericData.Record;
+
 /*
  * Takes input formated as: page title \t timestamp \t view count
  */
 public class HBaseKVMapper extends
-    Mapper<LongWritable, Text, ImmutableBytesWritable, KeyValue> {
-  ImmutableBytesWritable hKey = new ImmutableBytesWritable();
-  KeyValue kv;
+      Mapper<AvroKey<Record>, NullWritable, ImmutableBytesWritable, KeyValue> {
+  
+    ImmutableBytesWritable hKey = new ImmutableBytesWritable();
+    KeyValue kv;
+    public static final Log LOG = LogFactory.getLog(HBaseKVMapper.class);
 
-  @Override
-  protected void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-    String[] inputLine = value.toString().split("\t");
-    //String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
-    hKey.set(inputLine[0].getBytes());
-    context.write(hKey, new KeyValue(hKey.get(), "timestamps".getBytes(), 
-                inputLine[1].getBytes(), inputLine[2].getBytes()));
-  }
+    @Override
+    protected void map(AvroKey<Record> key, NullWritable value, Context context) 
+            throws IOException, InterruptedException {
+        
+        hKey.set(key.datum().get("pageTitle").toString().getBytes());
+        context.write(hKey, new KeyValue(hKey.get(), "timestamps".getBytes(),
+                key.datum().get("timestamp").toString().getBytes(), 
+                key.datum().get("count").toString().getBytes()));
+    }
 }
